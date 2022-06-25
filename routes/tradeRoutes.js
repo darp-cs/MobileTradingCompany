10627d43bbd8f44d0259f41020bcf50af553edda
Diego <Diegoarod14@gmail.com>
2022-06-25 16:40:59 -0400

    removed validation and rate limiter

--- routes/tradeRoutes.js
+++ routes/tradeRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/tradeController');
 const { isLoggedIn, isHost } = require('../middlewares/auth');
-const { validateId } = require('../middlewares/validator'); 
-const { validateMobile, validateResult} = require('../middlewares/validator');
 
 //GET /connections: send all connections to the user
 router.get('/', controller.index);
@@ -12,36 +10,36 @@ router.get('/', controller.index);
 router.get('/new', isLoggedIn, controller.new);
 
 //POST /connections: create a new connection
-router.post('/', isLoggedIn, validateMobile, validateResult, controller.create);
+router.post('/', isLoggedIn, controller.create);
 
 //GET /connections/:id: send details of connection identified by id
-router.get('/:id', validateId, controller.show);
+router.get('/:id',  controller.show);
 
 //GET /connections/:id: send html form for editing an existing connection
-router.get('/:id/edit', validateId, isLoggedIn, isHost, controller.edit);
+router.get('/:id/edit',  isLoggedIn, isHost, controller.edit);
 
 //PUT /connections/:id: update the connection identified by id
-router.put('/:id', validateId, isLoggedIn, isHost,validateMobile, validateResult, controller.update);
+router.put('/:id',  isLoggedIn, isHost, controller.update);
 
 //DELETE /connections/:id: delete the connection identified by id
-router.delete('/:id', validateId, isLoggedIn, isHost, controller.delete);
+router.delete('/:id',  isLoggedIn, isHost, controller.delete);
 
 
-router.post('/:id/interested', validateId, isLoggedIn, controller.interested);
+router.post('/:id/interested',  isLoggedIn, controller.interested);
 
-router.post('/:id/notinterested', validateId, isLoggedIn, controller.notinterested);
+router.post('/:id/notinterested',  isLoggedIn, controller.notinterested);
 
-router.post('/:id/tradeitem', validateId, isLoggedIn, controller.tradeitem);
+router.post('/:id/tradeitem',  isLoggedIn, controller.tradeitem);
 
-router.post('/:id/tradeoffered',validateId, isLoggedIn, controller.tradeoffered);
+router.post('/:id/tradeoffered', isLoggedIn, controller.tradeoffered);
 
-router.post('/:id/offercancel', validateId, isLoggedIn, controller.offercancel);
+router.post('/:id/offercancel',  isLoggedIn, controller.offercancel);
 
-router.post('/:id/manageOffer', validateId, isLoggedIn, controller.manageOffer);
+router.post('/:id/manageOffer',  isLoggedIn, controller.manageOffer);
 
-router.post('/:id/acceptOffer', validateId, isLoggedIn, controller.acceptOffer);
+router.post('/:id/acceptOffer',  isLoggedIn, controller.acceptOffer);
 
-router.post('/:id/rejectOffer', validateId, isLoggedIn, controller.rejectOffer);
+router.post('/:id/rejectOffer',  isLoggedIn, controller.rejectOffer);
 
 
 module.exports=router;  
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const controller = require('../controllers/userController');
 const { isGuest, isLoggedIn } = require('../middlewares/auth');
 // const{ logInLimiter } = require('../middlewares/rateLimiters');
-const { validateId } = require('../middlewares/validator'); 
-const{ validateSignUp, validateLogIn, validateResult} = require('../middlewares/validator');
 
 const router = express.Router();
 
@@ -13,13 +11,13 @@ router.get('/new', isGuest, controller.new);
 
 //POST /users: create a new user account
 
-router.post('/', isGuest, validateSignUp, validateResult,  controller.create);
+router.post('/', isGuest,  controller.create);
 
 //GET /users/login: send html for logging in
 router.get('/login', isGuest, controller.getUserLogin);
 
 //POST /users/login: authenticate user's login
-router.post('/login', isGuest, validateLogIn, validateResult, controller.login);
+router.post('/login', isGuest, controller.login);
 
 //GET /users/profile: send user's profile page
 router.get('/profile', isLoggedIn, controller.profile);
