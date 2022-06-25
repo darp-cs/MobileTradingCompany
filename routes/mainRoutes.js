4cb85aa298e32b5eb52ec4fbd98fc2cbbffb5abd
Tanvee5 <tanveeraje456@gmail.com>
2022-06-25 17:02:30 -0400

    Update mainRoutes.js

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/mainController');
+const mController = require('../controllers/mainController');
 
 
 //GET /: redirects to the home page
-router.get('/', controller.index);
+router.get('/', mController.homePage);
 
 //GET /contact: redirects to the contact page
-router.get('/contact', controller.contact);
+router.get('/contact', mController.contactPage);
 
 //POST /about: redirects to the about page
-router.get('/about', controller.about);
-//Change
+router.get('/about', mController.aboutPage);
+
 module.exports=router;  
